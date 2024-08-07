4f0f2d30a7f86392911b3f1bdacc173b572da93d
MarianoJuly <marcianocruzeiri@gmail.com>
2024-08-07 12:39:04 -0300

    simplificando js para ver caminho + necessidade de corrigir o html para atletas + necessario corrigir js para atletas

--- node_server/src/Main/app.js
+++ node_server/src/Main/app.js
@@ -1,3 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+app.post('/atletas', (req, res) => {
+  const dataset = req.body;
+  console.log('Dataset recebido:', dataset);
+  // Salvar ou processar o dataset aqui
+  res.status(200).send('Dataset recebido com sucesso');
+});
+
+app.use(express.static(path.join(__dirname, '../Front')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Front/forms.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+/*
+    Interação usuario
+*/
+
 document.getElementById('eventForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário
 
